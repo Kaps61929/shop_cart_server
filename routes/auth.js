5d8a05a16d15e056092438998a6396edd4455340
krish <kaps61929>
2023-12-21 12:36:25 +0530

    kk

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ authRouter.post("/api/signin", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).cache();
+    const user = await User.findOne({ email });
     if (!user) {
       return res
         .status(400)
